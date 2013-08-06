a6427287aca02b0bc4a886a661c61b9f28cd50af
ArnabGuin <arnab.guin@gmail.com>
2013-08-06 16:54:01 -0700

    aws scripts v1.0

---
+++ aws/credentials.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+aws_s3_bucket=<bucket name>
+aws_access_id=<AWS access id>
+aws_secret_key=<AWS secret key>
+aws_key_pair=<AWS key pair name>
+aws_key_pair_file=<AWS key pair file name>
+aws_log_uri=<S3 directory for logs>
+aws_region=<AWS region>
+aws_slave_instance_type=<slave instance type>
+aws_master_instance_type=<master instance type>
+aws_num_instances=<number of instances>
